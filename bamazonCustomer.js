e811d8ea1c47c05efef7d441fe0334731e1aeea2
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-10-01 23:24:24 -0500

    Started writing the logic for the prompts and responses but need to make things happen sequentially

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,13 @@
 var mysql = require("mysql");
 
+var inquirer = require("inquirer");
+
 var password = require("./password.js");
+
+var config = {
+    productChosen: NaN,
+    quantityToBuy: NaN
+};
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -9,19 +16,77 @@ var connection = mysql.createConnection({
 
     user: "root",
 
+    // Links to my password, which is kept in a different, gitignored JS file so that I don't put it in a public GitHub repo.
     password: password.password,
-    
+
     database: "bamazon"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw "error";
 
-    console.log("Connected. ", connection.threadId);
+    console.log("Connected.", connection.threadId);
 
     afterConnection();
 });
 
 function afterConnection() {
-    console.log();
+    displayAvailableItems();
+
+    userPurchaseQueries();
+
+    console.log("You purchased " + config.productChosen + ".\n\nYou bought " + config.quantityToBuy + " units.");
+};
+
+function displayAvailableItems() {
+    connection.query("SELECT * FROM PRODUCTS",
+
+        function (err, res) {
+            if (err) throw "Error.";
+
+            console.log(res);
+        });
+};
+
+function userPurchaseQueries() {
+    config.productChosen = whichProduct()
+        //.then(config.quantityToBuy = howManyUnits());
+};
+
+function whichProduct() {
+    inquirer
+        .prompt([
+            {
+                type: "number",
+                message: "Enter the ID number of the product you'd like to purchase.",
+                name: "chosenID"
+            }
+        ])
+        .then(answers => {
+            connection.query("SELECT * FROM products WHERE ?",
+                [
+                    {
+                        id: answers.chosenID
+                    }
+                ],
+                function (err, res) {
+                    if (err) throw "Error.";
+
+                    return res[0].id;
+                });
+        });
+};
+
+function howManyUnits() {
+    inquirer
+        .prompt([
+            {
+                type: "number",
+                message: "How many would you like to purchase?",
+                name: "quantityToBuy"
+            }
+        ])
+        .then(answers => {
+            return answers.quantityToBuy;
+        });
 };
