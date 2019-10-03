51864ace0a2cd0dfc951afc9db31c8aaafd62762
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-10-02 23:14:53 -0500

    Adding the mysql file for creating the database for grading reference

---
+++ bamazon.sql
@@ -0,0 +1,51 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+	id INT(10) NOT NULL AUTO_INCREMENT,
+    
+    product_name VARCHAR(50) NOT NULL,
+    
+    department_name VARCHAR(50) NOT NULL,
+    
+    price FLOAT(10,2) NOT NULL,
+    
+    stock_quantity INT(10) NOT NULL,
+    
+    PRIMARY KEY(id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Chrono Trigger", "Video Games", 39.99, 17);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dragon Quest VIII", "Video Games", 44.99, 21);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Planescape: Torment", "Video Games", 19.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Gravity's Rainbow", "Books", 9.99, 11);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Once and Future King", "Books", 14.99, 19);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("The Way of Kings", "Books", 14.99, 23);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Reading in the Dark: A Novel", "Books", 13.49, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Blade Runner", "Blu-ray", 8.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Lawrence of Arabia", "Blu-ray", 9.99, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Princess Mononoke", "Blu-ray", 7.99, 18);
+
+SELECT * FROM products;
